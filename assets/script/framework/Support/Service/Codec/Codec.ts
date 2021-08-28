e09567a9143ebf524d5bc84e45b4a1b9994b691d
Mr.Feng <fdh_1994@outlook.com>
2021-08-29 01:27:46 +0800

    更改MessageHeader 为Codec

--- assets/script/framework/Support/Service/Codec/Codec.ts
+++ assets/script/framework/Support/Service/Codec/Codec.ts
@@ -1,26 +1,21 @@
-export abstract class Codec {
+
+abstract class IMessage {
     //等待发送数据
     abstract get Data(): any
-
-    //编码数据
-    abstract Pack(data: any): boolean
-
-    //解码数据
-    abstract UnPack(data: any): boolean
-
     abstract get MsgID(): string
 }
 
+export abstract class Codec extends IMessage {
+    //编码数据
+    abstract Pack(data: any): boolean
+    //解码数据
+    abstract UnPack(data: any): boolean
+}
 
-export abstract class Message {
-    //等待处理数据
-    abstract get Data(): any
 
+export abstract class Message extends IMessage {
     //编码数据
     abstract Encode(): boolean
-
     //解码数据
     abstract Decode(data: any): boolean
-
-    abstract get MsgID(): string
 }
