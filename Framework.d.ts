0cb0decaf941049e8e91bcf9d226a3333f3cdc29
Mr.Feng <fdh_1994@outlook.com>
2021-09-09 21:38:33 +0800

    添加报错

--- Framework.d.ts
+++ Framework.d.ts
@@ -1,7 +1,13 @@
 declare let manager: import("./assets/script/Application").Application
 
+type GameEntry = import("./assets/script/Framework/Support/Entry/GameEntry").GameEntry
 declare type WebSocketType = "ws" | "wss";
 
 function dispatchEventWith(type: string, data?: any): void
 function dispatchModuleEvent(type: string, moduleName: string, param?: { onHided?: () => void, onShowed?: (error?: Error) => void, data?: any }): void
 function addEvent(obj: any, type: string, fun: Function)
+
+type IService = import("./assets/script/Framework/Defineds/Interfaces/IService").IService
+interface IServerClass {
+    readonly service: IService
+}
