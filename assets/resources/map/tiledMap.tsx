0f5857944efff1d9645a674cde420626d101f5b9
mumu <mumulei333@163.com>
2023-07-28 18:25:51 +0800

    init

---
+++ assets/resources/map/tiledMap.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="mateMap" tilewidth="32" tileheight="32" tilecount="144" columns="8">
+ <image source="./map.png" width="256" height="576"/>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0760234" y="0" width="30.924" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9" y="0" width="7" height="31"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/resources/script/MapController.ts
@@ -0,0 +1,48 @@
+import { _decorator, Camera, Component, find, instantiate, Node, Prefab, TiledMap, v3 } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MapController')
+export class MapController extends Component {
+
+
+    @property({ type: Prefab })
+    playerPrefab = null
+
+    map: TiledMap;
+
+    player: Node = null;
+
+
+    start() {
+        // 获取地图信息
+        this.map = this.getComponent(TiledMap);
+        // 普通层
+        // this.map.getLayer();
+        // 对象层
+        let playerLayer = this.map.getObjectGroup("playerLayer");
+        // 获取某个对象
+        let playerObj = playerLayer.getObject("startPos");
+        // 判断是否是玩家对象
+        // if (playerObj.isPlayer = true) {
+        //     // 加载玩家预设体,创建玩家
+        //     this.player = instantiate(this.playerPrefab);
+        //     this.player.parent = this.node.children[2].children[0];
+        //     this.player.position = v3(playerObj.x, playerObj.y);
+        // }
+    }
+
+    // 镜头聚焦主角(主角永远处于镜头中央)
+    cameraFocus() {
+        if (this.player != null) {
+            let position_x = this.player.position.x;
+            let position_y = this.player.position.y;
+            find("Canvas/Camera").position = v3(position_x, position_y);
+        }   
+    }
+
+    update(deltaTime: number) {
+        // this.cameraFocus();
+    }
+
+}
+
