389f58de1c252e393d8d789734aec7f5cda5415e
mumu <mumulei333@163.com>
2023-07-28 18:59:05 +0800

    update

--- assets/resources/script/MapController.ts
+++ assets/resources/script/MapController.ts
@@ -23,12 +23,13 @@ export class MapController extends Component {
         // 获取某个对象
         let playerObj = playerLayer.getObject("startPos");
         // 判断是否是玩家对象
-        // if (playerObj.isPlayer = true) {
-        //     // 加载玩家预设体,创建玩家
-        //     this.player = instantiate(this.playerPrefab);
-        //     this.player.parent = this.node.children[2].children[0];
-        //     this.player.position = v3(playerObj.x, playerObj.y);
-        // }
+        if (playerObj.isPlayer = true) {
+            // 加载玩家预设体,创建玩家
+            this.player = instantiate(this.playerPrefab);
+            this.player.parent = this.node.children[2].children[0];
+            this.player.active = true;
+            this.player.position = v3(playerObj.x, 400);
+        }
     }
 
     // 镜头聚焦主角(主角永远处于镜头中央)
@@ -41,7 +42,7 @@ export class MapController extends Component {
     }
 
     update(deltaTime: number) {
-        // this.cameraFocus();
+        this.cameraFocus();
     }
 
 }
